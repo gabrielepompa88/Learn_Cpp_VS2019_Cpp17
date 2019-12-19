6589db5f26a1ae8659df9497cf41784a2344cbff
gabrielepompa88 <gabriele.pompa@gmail.com>
2019-12-19 18:50:35 +0100

    doc updated - 2

--- Chap_4/Q4 - falling ball/Q4 - falling ball.cpp
+++ Chap_4/Q4 - falling ball/Q4 - falling ball.cpp
@@ -7,7 +7,7 @@
 int main()
 {
     const double H = getTowerHeight(); // height of the tower
-    double ht = H; // height at time t (at t==0 is H, the height of the tower)
+    double ht = H; // height of the ball at time t (at t==0 is H, the height of the tower)
     int t = 0; // time 
 
     while (ht > 0.0) {
