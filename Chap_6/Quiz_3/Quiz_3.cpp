70dd51052b374ec06e48d71eb2f574f8575fc58d
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-01-20 17:31:45 +0100

    passOrFail different from website solution

---
+++ Chap_6/Quiz_3/Quiz_3.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+
+bool passOrFail() {
+	static int s_user{ 1 };
+	return (s_user++ <= 3 ? true : false);
+}
+
+#include <iostream>
+int main()
+{
+	std::cout << "User #1: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #2: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #3: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #4: " << (passOrFail() ? "Pass\n" : "Fail\n");
+	std::cout << "User #5: " << (passOrFail() ? "Pass\n" : "Fail\n");
+
+	return 0;
+}
