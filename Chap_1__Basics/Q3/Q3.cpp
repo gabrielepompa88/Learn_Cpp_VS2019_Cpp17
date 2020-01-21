970bbd2bf8179ecb0b7f28d949a32f14582842c1
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-01-21 15:35:45 +0100

    folders renamed

---
+++ Chap_1__Basics/Q3/Q3.cpp
@@ -0,0 +1,19 @@
+
+#include <iostream>
+
+int main() {
+
+	int x{}, y{};
+
+	std::cout << "Enter an integer: ";
+	std::cin >> x;
+
+	std::cout << "Enter another integer: ";
+	std::cin >> y;
+
+	std::cout << "x + y = " << x << " + " << y << " = " << x + y << "\n";
+	std::cout << "x - y = " << x << " - " << y << " = " << x - y << "\n";
+
+	return 0;
+
+}
