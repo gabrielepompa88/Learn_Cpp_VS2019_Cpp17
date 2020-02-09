b3bec659531d138365871027a0c935a1424f7113
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-02-09 20:32:03 +0100

    IntArray class implemented

---
+++ Chap_9__Operator_Overloading/Quiz_3/Quiz_3.cpp
@@ -0,0 +1,111 @@
+#include <cassert> // for assert()
+#include <iostream>
+
+class IntArray {
+
+	int m_len;
+	int *m_arr;
+
+public:
+
+	IntArray(int len) : m_len(len) {
+		delete[] m_arr;
+		m_arr = new int[m_len];
+	}
+
+	void deepCopy(const IntArray& source_arr);
+
+	//(deep)copy constructor
+	IntArray(const IntArray& source_arr){
+		std::cout << "Copy constructor called\n";
+		deepCopy(source_arr);
+
+	}
+
+	//overloaded operator=
+	IntArray& operator=(const IntArray& source_arr) {
+		if (this == &source_arr)
+			return *this;
+
+		std::cout << "Overloaded operator= called\n";
+		deepCopy(source_arr);
+
+		return *this;
+
+	}
+
+	//overloaded operator[]
+	int& operator[](int i) const {
+		assert(i >= 0 && i < m_len && "Index out of range");
+
+		return m_arr[i];
+	}
+
+	friend std::ostream& operator<<(std::ostream& out, const IntArray& source_arr) {
+		for (int i = 0; i < source_arr.m_len; ++i) {
+			out << source_arr.m_arr[i] << " ";
+		}
+		return out;
+	}
+
+	//destructor
+	~IntArray() {
+		delete[] m_arr;
+	}
+};
+
+void IntArray::deepCopy(const IntArray& source_arr) {
+
+	// first we need to deallocate any value that this array is holding!
+	delete[] m_arr;
+
+	// because m_len is not a pointer, we can shallow copy it
+	m_len = source_arr.m_len;
+
+	// m_arr is a pointer, so we need to deep copy it if it is non-null
+	if (source_arr.m_arr)
+	{
+		// allocate memory for our copy
+		m_arr = new int[m_len];
+
+		// do the copy
+		for (int i{ 0 }; i < m_len; ++i)
+			m_arr[i] = source_arr.m_arr[i];
+	}
+	else
+		m_arr = nullptr;
+}
+
+IntArray fillArray()
+{
+	IntArray a(5);
+
+	a[0] = 5;
+	a[1] = 8;
+	a[2] = 2;
+	a[3] = 3;
+	a[4] = 6;
+
+	return a;
+}
+
+int main()
+{
+	std::cout << "IntArray a{ fillArray() }; --> ";
+	IntArray a{ fillArray() };
+	std::cout << a << '\n';
+
+	std::cout << "\nauto& ref{ a }; --> ";
+	auto& ref{ a }; // we're using this reference to avoid compiler self-assignment errors
+	std::cout << "\na = ref; --> ";
+	a = ref;
+
+	std::cout << "\nIntArray b(1); --> ";
+	IntArray b(1);
+	std::cout << "\nb = a; --> ";
+	b = a;
+
+	std::cout << b << '\n';
+
+	return 0;
+}
