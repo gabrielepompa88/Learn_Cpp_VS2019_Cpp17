e7988f48bebbadaaab5df9135e06fe5389c99620
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-09-17 18:09:08 +0100

    5c updated with fixed array passed by reference

--- Chap_P__Arrays_Strings_Pointers_and_References/Quiz_5c/Quiz_5c.cpp
+++ Chap_P__Arrays_Strings_Pointers_and_References/Quiz_5c/Quiz_5c.cpp
@@ -10,14 +10,28 @@ void printArray(const std::array<int, 5> &y)
     }
 }
 
+// overloaded function called when y is a standard array
+void printArray(int (&y)[5]) {
+
+    std::cout << "Calling overloaded printArray for standard arrays. \n";
+
+    for (int yi : y)
+    {
+        std::cout << yi << ' ';
+    }
+}
+
 int main()
 {
-    //int x[]{ 9, 7, 5, 3, 1 };
+    // using std::array
     std::array<int, 5> x{ 9, 7, 5, 3, 1 };
-
     printArray(x);
 
     std::cout << '\n';
+
+    // using fixed arrays
+    int z[]{ 9, 7, 5, 3, 1 };
+    printArray(z);
 
     return 0;
 }
