32b4cd00580259cfdb3bac9b0d0ac88b114e223d
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-09-17 17:30:32 +0100

    Quiz 3 added

---
+++ Chap_P__Arrays_Strings_Pointers_and_References/Quiz_3/Quiz_3.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+
+void swap(int& a, int& b) 
+{
+    std::cout << "Calling my own swap function." << "\n";
+
+    int temp{ a };
+
+    a = b;
+    b = temp;
+
+}
+
+
+int main()
+{
+    std::cout << "x = ";
+    int x{};
+    std::cin >> x;
+
+    int y{};
+    do
+    {
+        std::cout << "y = ";
+        std::cin >> y;
+    } while (x == y); // keep asking untill a different value is typed
+
+    std::cout << "x = " << x << "; y = " << y << "\n";
+
+    swap(x, y);
+
+    std::cout << "x = " << x << "; y = " << y << "\n";
+}
