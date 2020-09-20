fc1d4cae3026eb28d820a4acef50c85ad24ad246
gabrielepompa88 <gabriele.pompa@gmail.com>
2020-09-20 18:08:07 +0100

    Quiz 1 added

---
+++ Chap_F__Functions/Quiz_1/Quiz_1.cpp
@@ -0,0 +1,19 @@
+/*
+
+Write function prototypes for the following cases. Use const if/when necessary.
+
+a) A function named max() that takes two doubles and returns the larger of the two.
+
+double max(const double a, const double b);
+
+b) A function named swap() that swaps two integers.
+
+void swap(int &n, int &m);
+
+c) A function named getLargestElement() that takes a dynamically allocated array of integers 
+and returns the largest number in such a way that the caller can change the value of the element 
+returned (don’t forget the length parameter).
+
+int & getLargestElement(int *arr, int length);
+
+*/
